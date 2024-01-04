1801d073e2ae2ffe493fc0c6f14b97d57e92d1bf
Meet Vasani <w.vasanimeet@gmail.com>
2024-01-04 18:40:21 +0530

    added  security filter to protect routes

---
+++ src/main/java/com/sunbeam/filters/SecurityFilter.java
@@ -0,0 +1,40 @@
+package com.sunbeam.filters;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.sunbeam.pojos.User;
+
+//@WebFilter({"/candlist", "/vote", "/logout", "/result", "/candedit", "/canddel", "/announce"})
+public class SecurityFilter implements Filter {
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse resp = (HttpServletResponse) response;
+		
+		HttpSession session = req.getSession(false);
+		if(session != null) {
+			User user = (User) session.getAttribute("curUser");
+			if(user != null) {
+				// current request is by the authenticated user
+				chain.doFilter(request, response);
+				return;
+			}
+		}
+		// current request is by non-authenticated user
+		resp.sendRedirect("index.html");
+	}
+}
